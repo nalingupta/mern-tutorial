4542e70a71b2a8cdaa157860635015b613432330
Nalin Gupta <nalingupta.id@gmail.com>
2024-06-17 12:20:49 -0700

    chapter 3 half

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <Header className="header">
+        <div className="logo">
+            <Link to="/">GoalSetter</Link>
+        </div>
+    </Header>
+
+
+export default Header
